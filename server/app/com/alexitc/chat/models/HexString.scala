2fd5013d64d3e8e9c8cd9d3b9d43ff92f72821b7
Alexis Hernandez <alexis22229@gmail.com>
2019-03-24 14:10:57 -0700

    server: Add the HexString model

---
+++ server/app/com/alexitc/chat/models/HexString.scala
@@ -0,0 +1,25 @@
+package com.alexitc.chat.models
+
+import javax.xml.bind.DatatypeConverter
+
+import scala.util.Try
+
+class HexString private (val string: String) extends AnyVal {
+  def bytes: Array[Byte] = {
+    DatatypeConverter.parseHexBinary(string)
+  }
+}
+
+object HexString {
+
+  def apply(bytes: Array[Byte]): HexString = {
+    val string = DatatypeConverter.printHexBinary(bytes)
+    new HexString(string)
+  }
+
+  def from(string: String): Option[HexString] = {
+    Try { DatatypeConverter.parseHexBinary(string) }
+        .map(apply)
+        .toOption
+  }
+}
