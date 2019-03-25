773f551c416bff5fe70de86858c042cb6041b90d
Alexis Hernandez <alexis22229@gmail.com>
2019-03-24 20:58:09 -0700

    server: Add test using encryption

--- server/test/com/alexitc/chat/actors/PeerActorSpec.scala
+++ server/test/com/alexitc/chat/actors/PeerActorSpec.scala
@@ -1,8 +1,11 @@
 package com.alexitc.chat.actors
 
+import java.security.{PrivateKey, PublicKey}
+
 import akka.actor.{ActorSystem, PoisonPill}
 import akka.testkit.{TestKit, TestProbe}
-import com.alexitc.chat.models.{Channel, KeyPairs, Message, Peer}
+import com.alexitc.chat.models._
+import javax.crypto.Cipher
 import org.scalatest.MustMatchers._
 import org.scalatest.{BeforeAndAfterAll, WordSpecLike}
 
@@ -16,7 +19,8 @@ class PeerActorSpec
   }
 
   "the chat" must {
-    val dummyKey = Peer.Key(KeyPairs.generate().getPublic)
+    val aliceKeys = KeyPairs.generate()
+    val bobKeys = KeyPairs.generate()
     val channelHandler: ChannelHandlerActor.Ref = ChannelHandlerActor.Ref(
       system.actorOf(ChannelHandlerActor.props, "channel-handler")
     )
@@ -24,14 +28,12 @@ class PeerActorSpec
     val channelName = Channel.Name("test-channel")
 
     val aliceClient = TestProbe()
-    val alicePeer = Peer.Simple(Peer.Name("alice"), dummyKey)
+    val alicePeer = Peer.Simple(Peer.Name("alice"), Peer.Key(aliceKeys.getPublic))
     val alice = system.actorOf(PeerActor.props(aliceClient.ref, channelHandler))
 
     val bobClient = TestProbe()
-    val bobPeer = Peer.Simple(Peer.Name("bob"), dummyKey)
+    val bobPeer = Peer.Simple(Peer.Name("bob"), Peer.Key(bobKeys.getPublic))
     val bob = system.actorOf(PeerActor.props(bobClient.ref, channelHandler))
-
-    val message = Message.from("hola!")
 
     "allow alice to join" in {
       alice ! PeerActor.Command.JoinChannel(channelName, alicePeer)
@@ -47,7 +49,9 @@ class PeerActorSpec
       aliceClient.expectMsg(PeerActor.Event.PeerJoined(bobPeer))
     }
 
-    "allow alice to send a message to bob" in {
+    val plainTextMessage = "hola!"
+    val message = Message(Base64String.apply(encrypt(bobKeys.getPublic, plainTextMessage)))
+    "allow alice to send a message to bob encrypting it with bob's key" in {
       alice ! PeerActor.Command.SendMessage(bobPeer.name, message)
       bobClient.expectMsg(PeerActor.Event.MessageReceived(alicePeer, message))
     }
@@ -57,9 +61,30 @@ class PeerActorSpec
       aliceClient.expectMsg(PeerActor.Event.MessageReceived(bobPeer, message))
     }
 
+    "allow bob to decrypt the message from alice" in {
+      val text = decrypt(bobKeys.getPrivate, message.base64.bytes)
+      text must be(plainTextMessage)
+    }
+
     "notify bob when alice leaves" in {
       alice ! PoisonPill
       bobClient.expectMsg(PeerActor.Event.PeerLeft(alicePeer))
     }
   }
+
+  private def encrypt(key: PublicKey, message: String): Array[Byte] = {
+    val cipher = Cipher.getInstance("RSA")
+    cipher.init(Cipher.ENCRYPT_MODE, key)
+
+    cipher.doFinal(message.getBytes("UTF-8"))
+  }
+
+  private def decrypt(key: PrivateKey, message: Array[Byte]): String = {
+    val cipher = Cipher.getInstance("RSA")
+    cipher.init(Cipher.DECRYPT_MODE, key)
+
+    val bytes = cipher.doFinal(message)
+    new String(bytes)
+  }
+
 }
